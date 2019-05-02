54a30ab81868f6cd128d9b80c8c5d110cdefc915
erichou663 <houshaolong@gmail.com>
2019-05-02 11:12:11 +0800

    Demo

---
+++ src/com/web/autotest/SFWebAutoTestDemo.py
@@ -0,0 +1,17 @@
+'''
+Created on 2019年5月2日
+
+@author: erichou
+'''
+
+class SFWebAutoTestDemo(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
